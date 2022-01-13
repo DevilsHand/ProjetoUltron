6f887ce07f58e98d1ef9d348c5d350fd98f30cbd
Amanda Ribeiro Soares Belarmino <amanda.belarmino@inmetrics.com.br>
2022-01-13 16:32:43 -0300

    bash na pasta utils

--- src/main/java/creators/main.java
+++ src/main/java/creators/main.java
@@ -6,7 +6,7 @@ public class main {
 
 	public static void main(String[] args) {
 		String path = "C:\\Users\\gabriel.araujo\\Documents\\CMDer\\Cmder.exe"; 
-		String bash = "C:\\Users\\gabriel.araujo\\Documents\\CMDer\\vendor\\git-for-windows\\git-bash.exe";
+		String bash = "./utils/cmder/vendor/git-for-windows/git-bash.exe";
 		//ENFIA O CAMINHO DO SEU BASH NESSA VARIAVEL AKI ENCIMA
 		
 		String pathCmd = "C:\\Windows\\system32\\cmd.exe";
