2ca6573e44fca25ff534c457f9723c55b50b82ba
Gabriel Araujo Dos Santos <gabriel.araujo@inmetrics.com.br>
2022-01-20 14:48:34 -0300

    Push Automatizado

--- src/main/java/scripts/runScript.sh
+++ src/main/java/scripts/runScript.sh
@@ -5,3 +5,9 @@ echo -+--+--+--+--+--+--+--+--+--+--+--+--+--+-
 git add .
 git commit -m 'Push Automatizado'
 git push origin null
+echo -+--+--+--+--+--+--+--+--+--+--+--+--+--+-
+echo '|Insira sua senha do git no prompt abaixo|'
+echo -+--+--+--+--+--+--+--+--+--+--+--+--+--+-
+git add .
+git commit -m 'Push Automatizado'
+git push origin null
