b64ad16eab7cfb93dd23455c9186d70474ee548f
Gabriel Araujo Dos Santos <gabriel.araujo@inmetrics.com.br>
2022-02-21 09:31:30 -0300

    Adicionando função Git Clone - beta -

--- src/main/java/scripts/runScript.sh
+++ src/main/java/scripts/runScript.sh
@@ -1,3 +1,7 @@
 cd C:\\Users\\gabriel.araujo\\eclipse-workspace\\ProjetoUltron
 git add .
 git commit -m 'Adicionando função Git Clone - beta -'
+echo -+--+--+--+--+--+--+--+--+--+--+--+--+--+-
+echo '|Insira sua senha do git no prompt abaixo|'
+echo -+--+--+--+--+--+--+--+--+--+--+--+--+--+-
+git push origin Gabriel
